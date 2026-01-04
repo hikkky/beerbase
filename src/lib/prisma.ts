7eb9c5921f5cae2ad5e5fea99240a4ee47be6c06
hikkky <h.takaki@hupro-inc.com>
2026-01-04 11:21:43 +0900

    fix: simplify datasource URL configuration for PrismaClient

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -10,10 +10,7 @@ export const prisma =
   new PrismaClient({
     datasources: {
       db: {
-        url:
-          process.env.NODE_ENV === "production"
-            ? process.env.DATABASE_URL
-            : process.env.DATABASE_URL + "?pgbouncer=true",
+        url: process.env.DATABASE_URL,
       },
     },
   });
