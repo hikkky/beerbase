03ffc4b752ec0f39511135cefbbcb550a43d1d30
hikkky <h.takaki@hupro-inc.com>
2026-01-18 08:05:49 +0900

    feat: implement PostedBeers component to display user beer posts with images

--- src/app/(users)/users/[username]/_components/PostedBeers.tsx
+++ src/app/(users)/users/[username]/_components/PostedBeers.tsx
@@ -1,3 +1,5 @@
+import { getPostImageUrl } from "@/utils/getImageUrl";
+import Image from "next/image";
 import { BeerPost } from "../_data/fetchUserBeerPosts";
 
 type Props = {
@@ -5,5 +7,29 @@ type Props = {
 };
 
 export function PostedBeers({ beerPosts }: Props) {
-  return <div>PostedBeers</div>;
+  return (
+    <section className="grid grid-cols-3">
+      {beerPosts.map((beerPost) => {
+        const imageUrl = getPostImageUrl(beerPost.images[0]?.imageUrl || null); // TODO: 画像がない場合の処理
+        if (!imageUrl) return null;
+
+        return (
+          <article
+            key={beerPost.id.toString()}
+            className="relative w-full overflow-hidden"
+          >
+            <div className="relative w-full aspect-square">
+              <Image
+                src={imageUrl}
+                alt={beerPost.beerName}
+                fill
+                className="object-cover"
+                sizes="(min-width: 768px) 33vw, 100vw"
+              />
+            </div>
+          </article>
+        );
+      })}
+    </section>
+  );
 }
