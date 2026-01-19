177deebfa58085dad4698c0367da9be759dfb2e1
hikkky <h.takaki@hupro-inc.com>
2026-01-20 08:07:05 +0900

    feat: implement PostDetail component and fetchBeerPost function

---
+++ src/app/(posts)/posts/[postId]/_components/PostDetail.tsx
@@ -0,0 +1,9 @@
+import { BeerPost } from "../_data/fetchBeerPost";
+
+interface Props {
+  beerPost: BeerPost;
+}
+
+export function PostDetail({ beerPost }: Props) {
+  return <div>Post Detail Component</div>;
+}
---
+++ src/app/(posts)/posts/[postId]/_data/fetchBeerPost.ts
@@ -0,0 +1,47 @@
+import { prisma } from "@/lib/prisma";
+
+export type BeerPost = Awaited<ReturnType<typeof fetchBeerPost>>;
+
+export async function fetchBeerPost(postId: number) {
+  return prisma.beerPost.findUnique({
+    where: { id: postId },
+    select: {
+      id: true,
+      beerName: true,
+      breweryName: true,
+      abv: true,
+      ibu: true,
+      comment: true,
+      countryCode: true,
+      bodyScore: true,
+      sournessScore: true,
+      sweetnessScore: true,
+      aromaScore: true,
+      bitternessScore: true,
+      createdAt: true,
+      user: {
+        select: {
+          id: true,
+          user_profiles: {
+            select: {
+              username: true,
+              display_name: true,
+              avatar_url: true,
+            },
+          },
+        },
+      },
+      images: {
+        select: {
+          imageUrl: true,
+        },
+      },
+      style: {
+        select: {
+          name: true,
+        },
+      },
+      styleCustom: true,
+    },
+  });
+}
